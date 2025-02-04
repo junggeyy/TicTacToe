ba1937c6e7422496438b2ccefbc4e35550bdf796
junggeyy <jung.vickey@gmail.com>
2025-02-03 22:53:00 -0500

    Updates

---
+++ precache-manifest.48df36ee5c8685c8c238c60c42165773.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d7803ed082caee1661681a3b676b27e",
+    "url": "/TicTacToe/index.html"
+  },
+  {
+    "revision": "8cf9bc46e6712d8dc858",
+    "url": "/TicTacToe/static/css/main.47a33d50.chunk.css"
+  },
+  {
+    "revision": "56f1224220a5f06f9596",
+    "url": "/TicTacToe/static/js/2.e19d9236.chunk.js"
+  },
+  {
+    "revision": "8cf9bc46e6712d8dc858",
+    "url": "/TicTacToe/static/js/main.98112f5c.chunk.js"
+  },
+  {
+    "revision": "6670768c31502717d35c",
+    "url": "/TicTacToe/static/js/runtime~main.ae000fa4.js"
+  },
+  {
+    "revision": "60a09f200845d838f20e191f3790db25",
+    "url": "/TicTacToe/static/media/bg.60a09f20.jpg"
+  },
+  {
+    "revision": "e4fb02c79e61849a9a2cd207e8589bbc",
+    "url": "/TicTacToe/static/media/draw.e4fb02c7.wav"
+  },
+  {
+    "revision": "4a54ed977b87c1c48b6acf5ffff242c8",
+    "url": "/TicTacToe/static/media/lose.4a54ed97.mp3"
+  },
+  {
+    "revision": "544f858d46b11d84e3576983f4d5629f",
+    "url": "/TicTacToe/static/media/snake.544f858d.png"
+  },
+  {
+    "revision": "9bf439f79ba5ac99d1af77728690ba9b",
+    "url": "/TicTacToe/static/media/tada.9bf439f7.mp3"
+  },
+  {
+    "revision": "5aac49f9ab02bd5a8ac23b1603fb8c92",
+    "url": "/TicTacToe/static/media/tile.5aac49f9.wav"
+  }
+]);
